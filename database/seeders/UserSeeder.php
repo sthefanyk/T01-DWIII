20e5a002a17cc1b0e350e08810bdee25528d043e
sthefanyk <sthefanykmbrl@gmail.com>
2023-09-01 11:22:23 -0300

    aula 01/09

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    static $nomes = [
+        'Diego',
+        'Douglas',
+        'Visitante',
+        'Admin',
+    ];
+    static $emails = [
+        'diego@diego.com',
+        'douglas@douglas.com',
+        'visitante@visitante.com',
+        'admin@admin.com',
+    ];
+
+    public function run(){
+        for ($i = 0; $i < count(self::$nomes); $i++)
+            $user = User::create([
+                'name' => self::$nomes[$i],
+                'email' => self::$emails[$i],
+                'password' => Hash::make('123456789'),
+            ]);            
+        $user->save();
+    }
+}
